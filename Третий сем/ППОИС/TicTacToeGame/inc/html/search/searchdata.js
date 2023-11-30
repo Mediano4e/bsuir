45b0974f98a321f8facf20d5f94aeb5da0c2be32
Mediano4e <egor110704@gmail.com>
2023-11-30 11:40:40 +0300

    gf

---
+++ Третий сем/ППОИС/TicTacToeGame/inc/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "gt",
+  1: "gt"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files"
+};
+
